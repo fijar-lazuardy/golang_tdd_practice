6bca0671bcb37ed9a0449043d10b4240db682c54
Fijar Lazuardy <fijarlaz@gmail.com>
2022-10-24 15:56:10 +0700

    golang tdd practice

--- string_calculator.go
+++ string_calculator.go
@@ -1,5 +1,37 @@
 package tdd_golang
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 func Add(input string) (int, error) {
-	return 0, nil
+	var list_of_num []string
+	if input == "" {
+		return 0, nil
+	}
+	separated := strings.Split(input, "\n")
+	if len(separated[0]) == 1 || len(separated) == 1 {
+		remove_new_line := strings.ReplaceAll(input, "\n", ",")
+		list_of_num = strings.Split(remove_new_line, ",")
+	} else if len(separated[0]) > 1 && separated[0][:2] == "//" {
+		list_of_num = strings.Split(separated[1], separated[0][3:])
+
+	}
+
+	result := 0
+	for _, v := range list_of_num {
+		v_in_num, _ := strconv.Atoi(v)
+		if v_in_num < 0 {
+			err := fmt.Sprintf("negatives not allowed: %s", v)
+			return 0, errors.New(err)
+		} else if v_in_num > 1000 {
+			continue
+		}
+		result += v_in_num
+	}
+
+	return result, nil
 }
--- string_calculator_test.go
+++ string_calculator_test.go
@@ -1,13 +1,76 @@
 package tdd_golang_test
 
 import (
-	"github.com/supriyadi687/tdd-golang"
 	"testing"
+
+	tdd_golang "github.com/supriyadi687/tdd-golang"
 )
 
 func TestShouldReturn0GivenEmptyString(t *testing.T) {
 	res, _ := tdd_golang.Add("")
 	if res != 0 {
 		t.Errorf("expected to be 0 got %d", res)
+	}
+}
+
+func TestShouldReturnGivenNumber(t *testing.T) {
+	res, _ := tdd_golang.Add("1")
+	if res != 1 {
+		t.Errorf("Expected to be 1 but got %d", res)
+	}
+}
+
+func TestShouldReturn3GivenOneAndTwo(t *testing.T) {
+	res, _ := tdd_golang.Add("1,2")
+	if res != 3 {
+		t.Errorf("Wrong answer, expected 3 but got %d", res)
+	}
+}
+
+func TestInputShouldBeEmptyOneOrTwo(t *testing.T) {
+	_, err := tdd_golang.Add("1,2,3,4,5,5")
+	if err != nil {
+		t.Errorf("Should not return error")
+	}
+}
+
+func TestShouldAcceptCommaOrNewlineSeparatedString(t *testing.T) {
+	s := "1\n2,3"
+	res, _ := tdd_golang.Add(s)
+	if res != 6 {
+		t.Errorf("Expected 6 but got %d", res)
+	}
+}
+
+// func TestShouldNotAcceptCommaAndNewlineSideBySide(t *testing.T) {
+// 	s := "1,\n"
+// 	res, err := tdd_golang.Add(s)
+// 	if err == nil {
+// 		t.Errorf("Expected error appear but got %d", res)
+// 	}
+// }
+
+func TestGivingCustomDelimiter(t *testing.T) {
+	s := "//;\n1;2"
+	res, _ := tdd_golang.Add(s)
+	if res != 3 {
+		t.Errorf("Expected 3 but got %d", res)
+	}
+}
+
+func TestInputCannotBeNegative(t *testing.T) {
+	s := "1,-1,2"
+
+	res, err := tdd_golang.Add(s)
+	if err == nil {
+		t.Errorf("Expeceted error but got %d", res)
+	}
+}
+
+func TestIgnoreBigNumber(t *testing.T) {
+	s := "1,1002"
+	res, _ := tdd_golang.Add(s)
+	if res != 1 {
+		t.Errorf("Expected 1 but got %d", res)
 	}
 }
